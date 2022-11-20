f998355c488671224bbf5dc59fca6b001d64d518
BarloGER <barlozewski.mike@gmail.com>
2022-11-20 18:38:11 +0100

    added landing page with first design idea

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,12 @@ import "../styles/home.css";
 export default function Home() {
   return (
     <main className="home">
-      <div className="title">
+      <div className="content">
         <h1>
           Hi I'm <span>Mike Barlozewski</span> <br />
           I'm a Full Stack Web Developer
         </h1>
+        <button>Zu meinen Projekten</button>
       </div>
     </main>
   );
